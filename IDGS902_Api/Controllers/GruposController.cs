0fb030583bc5e4710c3e45028ae2729d61b57474
Carlos Ruiz <18002171@alumnos.utleon.edu.mx>
2023-07-10 20:52:23 -0600

    Agregar archivos de proyecto.

---
+++ IDGS902_Api/Controllers/GruposController.cs
@@ -0,0 +1,101 @@
+﻿using IDGS902_Api.Context;
+using IDGS902_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDGS902_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GruposController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public GruposController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.alumnos.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("{id}", Name = "alumnos")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var alum = _context.alumnos.FirstOrDefault(x => x.Id == id);
+                return Ok(alum);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Alumnos alum)
+        {
+            try
+            {
+                _context.alumnos.Add(alum);
+                _context.SaveChanges();
+                return CreatedAtRoute("alumnos", new { id = alum.Id }, alum);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Alumnos alum)
+        {
+            try
+            {
+                if (alum.Id == id)
+                {
+                    _context.Entry(alum).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return CreatedAtRoute("alumnos", new { id = alum.Id }, alum);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var alum = _context.alumnos.FirstOrDefault(alumnos => alumnos.Id == id);
+                if (alum != null)
+                {
+                    _context.alumnos.Remove(alum);
+                    _context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ IDGS902_Api/Startup.cs
@@ -0,0 +1,40 @@
+﻿using IDGS902_Api.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDGS902_Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                var frontedURL = Configuration.GetValue<string>("frontend_url");
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            services.AddControllers();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("conexion")));
+        }
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime)
+        {
+            //app.UseCors(); // Agregar el middleware de CORS
+             //app.UseRouting(); // Habilitar el enrutamiento
+
+            /*app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });*/
+        }
+    }
+}
